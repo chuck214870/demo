1841ca74528e2fd95fb08eb7ce618c85dc8f8031
chuck.white <cwhite_2@yahoo.com>
2017-07-31 20:19:40 -0400

    first commit of jiggy

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String index(Model model){
+		model.addAttribute("message", "Hello from the model!");
+		//return "Greetings from Eclipse Spring Boot!";
+		return "hello";
+	}
+
+}
