5fd0aa32f98dc2519e3456fe9c50be60837cc129
chuck.white <cwhite_2@yahoo.com>
2017-07-31 21:07:20 -0400

    Added CrudRepository.

--- src/main/java/com/example/demo/Greeting.java
+++ src/main/java/com/example/demo/Greeting.java
@@ -1,10 +1,17 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Greeting {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@NotNull
     @Min(1)
     private long id;
